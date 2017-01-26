6733c919137e9834d4c08d10b1e2d4d24af33b39
Adarnof <adarnof@gmail.com>
2017-01-26 10:46:43 -0500

    Handle version without installing

--- esi/__init__.py
+++ esi/__init__.py
@@ -3,5 +3,8 @@ from __future__ import unicode_literals
 default_app_config = 'esi.apps.EsiConfig'
 
 import pkg_resources
-__version__ = pkg_resources.require("adarnauth-esi")[0].version
+try:
+    __version__ = pkg_resources.get_distribution("aadarnauth-esi").version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'unknown'
 
